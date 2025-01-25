b79b802e259457df5c46df34162c984c860d4030
ItsSitanshu <sitanshu15shrestha@gmail.com>
2025-01-25 22:47:26 +0545

    Structure dashboard>patient sub pages

--- app/components/PatientTab.tsx
+++ app/components/PatientTab.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 interface PatientCardProps {
   user: any;
+  currentSubPage: number;
+  setCurrentSubPage: (value: number) => void;
 }
 
-const PatientTab: React.FC<PatientCardProps> = ({ user }) => {
+const PatientTab: React.FC<PatientCardProps> = ({ user, currentSubPage, setCurrentSubPage }) => {
   const getRandomColor = () => {
     const colors = ["4285F4", "EA4335", "FBBC05", "34A853"];
     return colors[Math.floor(Math.random() * colors.length)];
@@ -15,7 +17,12 @@ const PatientTab: React.FC<PatientCardProps> = ({ user }) => {
     userName
   )}&background=${getRandomColor()}&color=fff`;
 
-  return  (
+  const linkStyles = (index: number) =>
+    currentSubPage === index
+      ? 'border-b-2 border-foreground text-foreground' 
+      : 'border-b-2 border-transparent text-foreground/70 hover:text-foreground';
+
+  return (
     <div>
       <div className="flex items-center mb-6">
         <img
@@ -26,27 +33,27 @@ const PatientTab: React.FC<PatientCardProps> = ({ user }) => {
           width="80"
         />
         <div>
-          <h2 className="text-xl font-semibold text-black">{userName}</h2>
-          <div className="text-gray-500">hello</div>
+          <h2 className="text-xl font-semibold text-foreground">{userName}</h2>
+          <div className="text-foreground/50">hello</div>
         </div>
-        <button className="ml-auto bg-blue-600 text-white px-4 py-2 rounded-lg">
+        <button className="ml-auto bg-foreground text-background px-4 py-2 rounded-lg hover:bg-foreground/80">
           Add Note
         </button>
       </div>
-      <div className="border-b border-gray-200 mb-6">
+      <div className="border-b border-foreground/10 mb-6">
         <div className="flex">
-          <div className="mr-6 pb-2 border-b-2 border-transparent">
-            <a className="text-gray-600" href="/dashboard/patient/patientinfo">
+          <div className={`mr-6 pb-2 ${linkStyles(0)}`} onClick={() => setCurrentSubPage(0)}>
+            <a className="text-sm">
               Patient Information
             </a>
           </div>
-          <div className="mr-6 pb-2 border-b-2 border-transparent">
-            <a className="text-gray-600" href="#">
+          <div className={`mr-6 pb-2 ${linkStyles(1)}`} onClick={() => setCurrentSubPage(1)}>
+            <a className="text-sm" href="#">
               Appointment History
             </a>
           </div>
-          <div className="pb-2 border-b-2 border-blue-600">
-            <a className="text-blue-600" href="#">
+          <div className={`pb-2 ${linkStyles(2)}`} onClick={() => setCurrentSubPage(2)}>
+            <a className="text-sm" href="#">
               Medical Record
             </a>
           </div>
--- app/dashboard/patient/page.tsx
+++ app/dashboard/patient/page.tsx
@@ -5,12 +5,17 @@ import DashboardSidebar from "@/app/components/DashboardSidebar";
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 import PatientTab from '@/app/components/PatientTab'; 
 import Navbar from "@/app/components/Navbar";
+import MedicalRecord from "@/app/components/MedicalRecord";
+import PatientInformation from "@/app/components/PatitentInformation";
+import AppointmentHistory from "@/app/components/AppointmentHistory";
 
 const supabase = createClientComponentClient();
 const SUB_PAGE_NAME = "Patient";
 
 export default function DashboardPatientPage() {
   const [user, setUser] = useState<any>();
+
+  const [subSection, setSubSection] = useState<number>(0);
 
   useEffect(() => {
     const fetchSession = async () => {
@@ -51,104 +56,8 @@ export default function DashboardPatientPage() {
               
               <div>
                 <div className="flex mb-6"></div>
-                <PatientTab user={user}/>
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                  {/* First duplicated item */}
-                  <div className="bg-gray-50 p-6 rounded-lg shadow">
-                    <h3 className="text-lg font-semibold mb-4 text-black">Maxillary Left Lateral Incisor</h3>
-                    <div className="mb-4">
-                      <div className="flex justify-between items-center mb-2">
-                        <div className="text-gray-500">MEI 03</div>
-                        <div className="text-green-600">Done</div>
-                      </div>
-                      <div className="bg-white p-4 rounded-lg shadow">
-                        <div className="flex justify-between items-center mb-2">
-                          <div className="text-gray-600">Condition</div>
-                          <div className="text-gray-600">Caries</div>
-                        </div>
-                        <div className="flex justify-between items-center mb-2">
-                          <div className="text-gray-600">Treatment</div>
-                          <div className="text-gray-600">Tooth filling</div>
-                        </div>
-                        <div className="flex justify-between items-center mb-2">
-                          <div className="text-gray-600">Dentist</div>
-                          <div className="text-gray-600">Drg Soap Mactavish</div>
-                        </div>
-                        <div className="text-gray-500">Advanced Decay</div>
-                      </div>
-                    </div>
-                    <div>
-                      <div className="flex justify-between items-center mb-2">
-                        <div className="text-gray-500">APR 12</div>
-                        <div className="text-yellow-600">Pending</div>
-                      </div>
-                      <div className="bg-white p-4 rounded-lg shadow">
-                        <div className="flex justify-between items-center mb-2">
-                          <div className="text-gray-600">Condition</div>
-                          <div className="text-gray-600">Caries</div>
-                        </div>
-                        <div className="flex justify-between items-center mb-2">
-                          <div className="text-gray-600">Treatment</div>
-                          <div className="text-gray-600">Tooth filling</div>
-                        </div>
-                        <div className="flex justify-between items-center mb-2">
-                          <div className="text-gray-600">Dentist</div>
-                          <div className="text-gray-600">Drg Soap Mactavish</div>
-                        </div>
-                        <div className="text-gray-500">Reason: Not enough time</div>
-                        <div className="text-gray-500">Decay in pulp</div>
-                      </div>
-                    </div>
-                  </div>
-
-                  {/* Second duplicated item */}
-                  <div className="bg-gray-50 p-6 rounded-lg shadow">
-                    <h3 className="text-lg font-semibold mb-4 text-black">Maxillary Left Lateral Incisor</h3>
-                    <div className="mb-4">
-                      <div className="flex justify-between items-center mb-2">
-                        <div className="text-gray-500">MEI 03</div>
-                        <div className="text-green-600">Done</div>
-                      </div>
-                      <div className="bg-white p-4 rounded-lg shadow">
-                        <div className="flex justify-between items-center mb-2">
-                          <div className="text-gray-600">Condition</div>
-                          <div className="text-gray-600">Caries</div>
-                        </div>
-                        <div className="flex justify-between items-center mb-2">
-                          <div className="text-gray-600">Treatment</div>
-                          <div className="text-gray-600">Tooth filling</div>
-                        </div>
-                        <div className="flex justify-between items-center mb-2">
-                          <div className="text-gray-600">Dentist</div>
-                          <div className="text-gray-600">Drg Soap Mactavish</div>
-                        </div>
-                        <div className="text-gray-500">Advanced Decay</div>
-                      </div>
-                    </div>
-                    <div>
-                      <div className="flex justify-between items-center mb-2">
-                        <div className="text-gray-500">APR 12</div>
-                        <div className="text-yellow-600">Pending</div>
-                      </div>
-                      <div className="bg-white p-4 rounded-lg shadow">
-                        <div className="flex justify-between items-center mb-2">
-                          <div className="text-gray-600">Condition</div>
-                          <div className="text-gray-600">Caries</div>
-                        </div>
-                        <div className="flex justify-between items-center mb-2">
-                          <div className="text-gray-600">Treatment</div>
-                          <div className="text-gray-600">Tooth filling</div>
-                        </div>
-                        <div className="flex justify-between items-center mb-2">
-                          <div className="text-gray-600">Dentist</div>
-                          <div className="text-gray-600">Drg Soap Mactavish</div>
-                        </div>
-                        <div className="text-gray-500">Reason: Not enough time</div>
-                        <div className="text-gray-500">Decay in pulp</div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+                <PatientTab user={user} currentSubPage={subSection} setCurrentSubPage={setSubSection}/>
+                { subSection == 2 ? <MedicalRecord/> : subSection == 1 ? <AppointmentHistory/> : subSection == 0 ? <PatientInformation/> : <></> }
               </div>
             </div>
           </div>
